e3e6d7381276e27a8e44a4da701f754666c13175
ketang <509634578tk@gmail.com>
2023-06-04 12:26:23 +0800

    feat: fix XML version - Modify the specific xml version number to open the file using the official logisim-evolution in macos

---
+++ ketang/sp21/fix_logisim_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!/bin/bash
+
+
+#!/bin/bash
+
+
+#!/bin/bash
+
+#!/bin/bash
+
+find . -name '*.circ' -exec sed -i '' -E 's/(source="3\.4\.3)-[^"]*/\1/g' {} \; -exec echo "Modified file: {}" \;
+
